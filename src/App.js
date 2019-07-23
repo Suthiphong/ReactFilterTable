e666727b16f4559be23b21d1728232b427a480ca
Jingjo_D <kitty.username@hotmail.com>
2019-07-23 22:56:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Data from './Data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      UserData: Data,
+      input: ''
+    }
+    
+  }
+
+  CreateTable = () => {
+    let {UserData,input} = this.state
+    return UserData.filter(item =>item.first_name.includes(input)).map( (item ,index ) => {
+        return (
+          <tr>
+            <td key={index} style={{textAlign:'center'}}>{item.id}</td>
+            <td style={{textAlign:'left'}}>{item.first_name}</td>
+            <td >{item.last_name}</td>
+            <td>{item.ip_address}</td>
+          </tr>
+        )
+    })
+  }
+
+
+    
+  
+
+  render(){
+    return(
+      <div>
+        <label style={{fontSize:50}}>Table Filter</label>
+        <hr style={{margin:20}}></hr>
+        <div style={{margin: 25}}>
+        <select onChange={(e)=> this.setState({input: e.target.value})}>
+          {this.state.UserData.map(item => (
+            <option>{item.first_name}</option>
+          ))}
+        </select>
+        
+        <input placeholder="Search." style={{margin:5,fontSize:20,borderRadius:10}} onChange={(e)=>this.setState({input: e.target.value})}/> Now filter =>  {this.state.input}
+        
+          <table className="hoverTable">
+          <tr>
+            <th style={{width:70}}>ID</th>
+            <th style={{width:150, textAlign:'left'}}>first name</th>
+            <th style={{width:150, textAlign:'left'}}>Last name</th>
+            <th style={{width:200, textAlign: 'left'}}>ip address</th>
+          </tr>
+          {this.CreateTable()}
+          </table>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
